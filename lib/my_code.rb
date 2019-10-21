037e05685acae407bd3333598b149f1ad89b09fa
Will Drougas <wdrougas@gmail.com>
2019-10-21 11:32:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,28 +1,16 @@
-def map(s)
+def map(array)
   new = []
   i = 0
-  while i < s.length
-    new.push(yield(s[i]))
+  while i < array.length do
+    new.push(yield(array[i]))
     i += 1
   end
   new
 end
 
 
-def reduce(s, sp=nil)
-  if sp
-    accum = sp
-    i = 0
-  else
-    accum = s[0]
-    i = 1
-  end
-  while i < s.length
-    accum = yield(accum, s[i])
-    i += 1
-  end
-  accum
-end
-    
+def reduce(array)
   
+end
+
   
